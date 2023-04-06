4f4755523ec67284a9ac363b691d4dc3b244e470
Pavel Klyuev <71312774+PavelKlyuev@users.noreply.github.com>
2023-04-06 12:04:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,10 +6,9 @@ from scipy.stats import expon
 
 chat_id = 240671999 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
+def exact_solution(p: float, x: np.array) -> float:
     t = 32
-    s = np.array(list(map(lambda xi: (xi / t) / t, x)))
     alpha = 1 - p
-    return expon.ppf(alpha / 2) / len(s) * s.min(), \
-           expon.ppf(1 - alpha / 2) / len(s) * s.min()
+    return expon.ppf(alpha / 2) / len(x) * (x.min()/t), \
+           expon.ppf(1 - alpha / 2) / len(x) * (x.min()/t)
 
