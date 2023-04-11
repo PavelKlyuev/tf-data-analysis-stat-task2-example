0b85344594b7632f4cb01ec97be61243b36c2e83
Pavel Klyuev <71312774+PavelKlyuev@users.noreply.github.com>
2023-04-11 10:29:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,9 @@ from scipy.stats import expon
 chat_id = 240671999 # Ваш chat ID, не меняйте название переменной
 
 def exact_solution(p: float, x: np.array) -> float:
-    t = 32
-    alpha = 1 - p
-    return expon.ppf(alpha / 2) / len(x) * (x.min()/t), \
-           expon.ppf(1 - alpha / 2) / len(x) * (x.min()/t)
+        alpha = 1 - p
+        loc = (x.mean() - 0.5) / 512.0
+        scale = 1 / (512.0 * len(x))
+        return gamma.ppf(alpha / 2, len(x), loc=loc, scale=scale), \
+               gamma.ppf(1 - alpha / 2, len(x), loc=loc, scale=scale)
 
